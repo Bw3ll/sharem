abed11c69659cbb2d519ea5e09c8635eae24afc5
DSU-ah <dsu.verona.ah@gmail.com>
2021-06-25 12:10:08 -0500

    Create ui.py

---
+++ ui.py
@@ -0,0 +1,25 @@
+# -32 or 64 bit mode
+# -Option to ignore syscall
+# -Option for each of the types
+#     -FSTENV
+#     -Callpop
+#     -PebSequences
+#     -Pushret
+#     -PushStrings
+#     -Stringsonsections
+#     -getDLLs
+#	  -get Peb walk start
+#     -getSyscall
+#     -get Egghunters
+#     -get heaven
+# -Different registers
+# -Options 
+import sys
+def sp():
+	sys.stdout.flush()
+def showOptions():
+	sp()
+	options = "\nOptions:\n"
+	options += "x: Exit\n"
+	print(options, flush=True)
+	sp()
