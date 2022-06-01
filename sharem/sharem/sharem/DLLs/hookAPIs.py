8934fbc407ea583114e580692fc83f8820808783
Ibranum <evan.read@trojans.dsu.edu>
2022-06-01 09:47:47 -0500

    SetClipboardData Function
    Added Handle Type, added the function itself "SetClipboardData". Returns working with the shellcode values provided to it.

--- sharem/sharem/sharem/DLLs/hookAPIs.py
+++ sharem/sharem/sharem/DLLs/hookAPIs.py
@@ -74,6 +74,7 @@ class HandleType(Enum):
     HGLOBAL = auto()
     # Module
     HMODULE = auto()
+    ClipBoard = auto()
 
 
 class Handle:
@@ -4887,6 +4888,28 @@ class CustomWinAPIs():
         uc.reg_write(UC_X86_REG_EAX, retVal)
 
         logged_calls = ("IsDebuggerPresent", hex(callAddr), (retValStr), 'BOOL', pVals, pTypes, pNames, False)
+        return logged_calls, cleanBytes
+
+    def SetClipboardData(self, uc, eip, esp, export_dict, callAddr, em):
+        pTypes =['UINT', 'HANDLE'] 
+        pNames = ['uFormat', 'hMem'] 
+        pVals = makeArgVals(uc, em, esp, len(pTypes))
+        # ClipBoard = auto() handle type at top
+        FormatReverseLookUp = {2: 'CF_BITMAP', 8: 'CF_DIB', 17: 'CF_DIBV5', 5: 'CF_DIF', 130: 'CF_DSPBITMAP', 142: 'CF_DSPENHMETAFILE', 131: 'CF_DSPMETAFILEPICT', 129: 'CF_DSPTEXT', 14: 'CF_ENHMETAFILE', 768: 'CF_GDIOBJFIRST', 1023: 'CF_GDIOBJLAST', 15: 'CF_HDROP', 16: 'CF_LOCALE', 3: 'CF_METAFILEPICT', 7: 'CF_OEMTEXT', 128: 'CF_OWNERDISPLAY', 9: 'CF_PALETTE', 10: 'CF_PENDATA', 512: 'CF_PRIVATEFIRST', 767: 'CF_PRIVATELAST', 11: 'CF_RIFF', 4: 'CF_SYLK', 1: 'CF_TEXT', 6: 'CF_TIFF', 13: 'CF_UNICODETEXT', 12: 'CF_WAVE'}
+
+        pVals[0] = getLookUpVal(pVals[0], FormatReverseLookUp)
+
+        skip = [0]
+        pTypes,pVals= findStringsParms(uc, pTypes,pVals, skip)
+
+        handle = Handle(HandleType.Clipboard)
+
+        cleanBytes = cleanBytes = stackCleanup(uc, em, esp, len(pTypes))
+        retVal =  handle.value # if success, return val is handle to data
+        retValStr = hex(retVal)
+        uc.reg_write(UC_X86_REG_EAX, retVal)
+    
+        logged_calls= ("SetClipboardData", hex(callAddr), (retValStr), 'HANDLE', pVals, pTypes, pNames, False)
         return logged_calls, cleanBytes
 
     def SetLastError(self, uc, eip, esp, export_dict, callAddr, em):
