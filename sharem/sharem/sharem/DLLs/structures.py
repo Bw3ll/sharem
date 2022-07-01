52d58b2c05da83e21bd87325b14204f3aadcc3a2
ShelbyVH <77289500+ShelbyVH@users.noreply.github.com>
2022-07-01 07:49:36 -0500

    Removed Type Alias using long/ulong because of size differences between linux and windows.

--- sharem/sharem/sharem/DLLs/structures.py
+++ sharem/sharem/sharem/DLLs/structures.py
@@ -1,4 +1,4 @@
-from ctypes import LittleEndianStructure, c_byte, c_char, c_double, c_float, c_int, c_long, c_longlong, c_short, c_uint, c_ulong, c_ulonglong, c_ushort, c_wchar, sizeof
+from ctypes import LittleEndianStructure, c_byte, c_char, c_double, c_float, c_int, c_int32, c_int64, c_longlong, c_short, c_uint, c_uint32, c_uint64, c_ulonglong, c_ushort, c_wchar, sizeof
 from enum import Enum
 from struct import pack, unpack
 from time import gmtime, localtime,time_ns, localtime
@@ -13,13 +13,13 @@ MAX_PATH = 260
 # General Types
 BYTE = c_byte
 WORD = c_ushort
-DWORD = c_long
+DWORD = c_uint32
 
 CHAR = c_char
 WCHAR = c_wchar
 
 BOOLEAN = BYTE
-BOOL = c_long
+BOOL = c_uint32
 
 USHORT = c_ushort
 SHORT = c_short
@@ -27,8 +27,8 @@ SHORT = c_short
 UINT = c_uint
 INT = c_int
 
-ULONG = c_ulong
-LONG = c_long
+ULONG = c_uint32 # Windows LONG is 4 bytes
+LONG = c_int32 # Unix LONG is 8 bytes
 
 ULONGLONG = c_ulonglong
 LONGLONG = c_longlong
@@ -37,22 +37,22 @@ DOUBLE = c_double
 FLOAT = c_float
 
 # 32 Bit Pointers / Const Pointers
-LPCWSTR_32BIT = LPWSTR_32BIT = PWSTR_32BIT = c_ulong
-LPCSTR_32BIT = LPSTR_32BIT = PSTR_32BIT = c_ulong
-LPCVOID_32BIT = LPVOID_32BIT = PVOID_32BIT = c_ulong
+LPCWSTR_32BIT = LPWSTR_32BIT = PWSTR_32BIT = c_uint32
+LPCSTR_32BIT = LPSTR_32BIT = PSTR_32BIT = c_uint32
+LPCVOID_32BIT = LPVOID_32BIT = PVOID_32BIT = c_uint32
 
 # 64 Bit Pointers / Const Pointers
-LPCWSTR_64BIT = LPWSTR_64BIT = PWSTR_64BIT = c_ulonglong
-LPCSTR_64BIT = LPSTR_64BIT = PSTR_64BIT = c_ulonglong
-LPCVOID_64BIT = LPVOID_64BIT = PVOID_64BIT = c_ulonglong
+LPCWSTR_64BIT = LPWSTR_64BIT = PWSTR_64BIT = c_uint64
+LPCSTR_64BIT = LPSTR_64BIT = PSTR_64BIT = c_uint64
+LPCVOID_64BIT = LPVOID_64BIT = PVOID_64BIT = c_uint64
 
 # Params 32 Bit 
-WPARAM_32BIT = c_ulong
-LPARAM_32BIT = c_long
+WPARAM_32BIT = c_uint32
+LPARAM_32BIT = c_int32
 
 # Params 64 Bit
-WPARAM_64BIT = c_ulonglong
-LPARAM_64BIT = c_longlong
+WPARAM_64BIT = c_uint64
+LPARAM_64BIT = c_int64
 
 ATOM = WORD
 LANGID = WORD
@@ -62,27 +62,29 @@ LGRPID = DWORD
 LCTYPE = DWORD
 LCID = DWORD
 
+DWORD_PTR = c_uint32
+
 # Handles 32 Bit
-HANDLE_32BIT = c_ulong # Base Handle
+HANDLE_32BIT = c_uint32 # Base Handle
 
 HACCEL_32BIT = HBITMAP_32BIT = HBRUSH_32BIT = HCOLORSPACE_32BIT = HDC_32BIT = HDESK_32BIT = HDWP_32BIT = HENHMETAFILE_32BIT = HFONT_32BIT = HGDIOBJ_32BIT = HGLOBAL_32BIT = HHOOK_32BIT = HICON_32BIT = HINSTANCE_32BIT = HKEY_32BIT = HKL_32BIT = HLOCAL_32BIT = HMENU_32BIT = HMETAFILE_32BIT = HMODULE_32BIT = HMONITOR_32BIT = HPALETTE_32BIT = HPEN_32BIT = HRGN_32BIT = HRSRC_32BIT = HSTR_32BIT = HTASK_32BIT = HWINSTA_32BIT = HWND_32BIT = SC_HANDLE_32BIT = SERVICE_STATUS_HANDLE_32BIT = HANDLE_32BIT
 
 # Handles 64 Bit
-HANDLE_64BIT = c_ulonglong # Base Handle
+HANDLE_64BIT = c_uint64 # Base Handle
 
 HACCEL_64BIT = HBITMAP_64BIT = HBRUSH_64BIT = HCOLORSPACE_64BIT = HDC_64BIT = HDESK_64BIT = HDWP_64BIT = HENHMETAFILE_64BIT = HFONT_64BIT = HGDIOBJ_64BIT = HGLOBAL_64BIT = HHOOK_64BIT = HICON_64BIT = HINSTANCE_64BIT = HKEY_64BIT = HKL_64BIT = HLOCAL_64BIT = HMENU_64BIT = HMETAFILE_64BIT = HMODULE_64BIT = HMONITOR_64BIT = HPALETTE_64BIT = HPEN_64BIT = HRGN_64BIT = HRSRC_64BIT = HSTR_64BIT = HTASK_64BIT = HWINSTA_64BIT = HWND_64BIT = SC_HANDLE_64BIT = SERVICE_STATUS_HANDLE_64BIT = HANDLE_64BIT
 
 # Pointer to Types / Handles 32 Bit
-LPBOOL_32BIT = PBOOL_32BIT = PBOOLEAN_32BIT = LPBYTE_32BIT = PBYTE_32BIT = PCHAR_32BIT = LPCOLORREF_32BIT = LPDWORD_32BIT = PDWORD_32BIT = LPFILETIME_32BIT = PFILETIME_32BIT = PFLOAT_32BIT = LPHANDLE_32BIT = PHANDLE_32BIT = PHKEY_32BIT = LPHKL_32BIT = LPINT_32BIT = PINT_32BIT = PLCID_32BIT = LPLONG_32BIT = PLONG_32BIT = LPSC_HANDLE_32BIT = PSHORT_32BIT = LPUINT_32BIT = PUINT_32BIT = PULONG_32BIT = PUSHORT_32BIT = PWCHAR_32BIT = LPWORD_32BIT = PWORD_32BIT = c_ulong
+LPBOOL_32BIT = PBOOL_32BIT = PBOOLEAN_32BIT = LPBYTE_32BIT = PBYTE_32BIT = PCHAR_32BIT = LPCOLORREF_32BIT = LPDWORD_32BIT = PDWORD_32BIT = LPFILETIME_32BIT = PFILETIME_32BIT = PFLOAT_32BIT = LPHANDLE_32BIT = PHANDLE_32BIT = PHKEY_32BIT = LPHKL_32BIT = LPINT_32BIT = PINT_32BIT = PLCID_32BIT = LPLONG_32BIT = PLONG_32BIT = LPSC_HANDLE_32BIT = PSHORT_32BIT = LPUINT_32BIT = PUINT_32BIT = PULONG_32BIT = PUSHORT_32BIT = PWCHAR_32BIT = LPWORD_32BIT = PWORD_32BIT = c_uint32
 
 # Pointer to Types / Handles 64 Bit
-LPBOOL_64BIT = PBOOL_64BIT = PBOOLEAN_64BIT = LPBYTE_64BIT = PBYTE_64BIT = PCHAR_64BIT = LPCOLORREF_64BIT = LPDWORD_64BIT = PDWORD_64BIT = LPFILETIME_64BIT = PFILETIME_64BIT = PFLOAT_64BIT = LPHANDLE_64BIT = PHANDLE_64BIT = PHKEY_64BIT = LPHKL_64BIT = LPINT_64BIT = PINT_64BIT = PLCID_64BIT = LPLONG_64BIT = PLONG_64BIT = LPSC_HANDLE_64BIT = PSHORT_64BIT = LPUINT_64BIT = PUINT_64BIT = PULONG_64BIT = PUSHORT_64BIT = PWCHAR_64BIT = LPWORD_64BIT = PWORD_64BIT = c_ulonglong
+LPBOOL_64BIT = PBOOL_64BIT = PBOOLEAN_64BIT = LPBYTE_64BIT = PBYTE_64BIT = PCHAR_64BIT = LPCOLORREF_64BIT = LPDWORD_64BIT = PDWORD_64BIT = LPFILETIME_64BIT = PFILETIME_64BIT = PFLOAT_64BIT = LPHANDLE_64BIT = PHANDLE_64BIT = PHKEY_64BIT = LPHKL_64BIT = LPINT_64BIT = PINT_64BIT = PLCID_64BIT = LPLONG_64BIT = PLONG_64BIT = LPSC_HANDLE_64BIT = PSHORT_64BIT = LPUINT_64BIT = PUINT_64BIT = PULONG_64BIT = PUSHORT_64BIT = PWCHAR_64BIT = LPWORD_64BIT = PWORD_64BIT = c_uint64
 
 # Pointers to Structures 32 Bit
-PUNICODE_STRING_32BIT = c_ulong
+PUNICODE_STRING_32BIT = c_uint32
 
 # Pointers to Structures 64 Bit
-PUNICODE_STRING_64BIT = c_ulonglong
+PUNICODE_STRING_64BIT = c_uint64
 
 # Helpers
 def read_string(uc: Uc, address: int):
@@ -485,6 +487,8 @@ class Structure:
             self.dwHighDateTime = unPacked[1]
 
     def get_UNICODE_STRING(uc: Uc, address: int, em):
+        print('32 Size: ', sizeof(Structure.UNICODE_STRING.ARCH32))
+        print('64 Size: ', sizeof(Structure.UNICODE_STRING.ARCH64))
         if em.arch == 32:
             return Structure.UNICODE_STRING.ARCH32.from_buffer_copy(uc.mem_read(address, sizeof(Structure.UNICODE_STRING.ARCH32)))
         else:
@@ -657,3 +661,66 @@ class Structure:
         def writeToMemory(self, uc: Uc, address: int):
             uc.mem_write(address, bytes(self))
 
+
+    # Struct SECURITY_ATTRIBUTES
+    # Alias Names:
+    # Alias Pointer Names: *PSECURITY_ATTRIBUTES, *LPSECURITY_ATTRIBUTES
+
+    def get_SECURITY_ATTRIBUTES(uc: Uc, address: int, em):
+            if em.arch == 32:
+                return Structure.SECURITY_ATTRIBUTES.ARCH32.from_buffer_copy(uc.mem_read(address, sizeof(Structure.SECURITY_ATTRIBUTES.ARCH32)))
+            else:
+                return Structure.SECURITY_ATTRIBUTES.ARCH64.from_buffer_copy(uc.mem_read(address, sizeof(Structure.SECURITY_ATTRIBUTES.ARCH64)))
+
+    class SECURITY_ATTRIBUTES:
+
+        class ARCH32(LittleEndianStructure):
+            types = ['DWORD', 'LPVOID', 'BOOL']
+            names = ['nLength', 'lpSecurityDescriptor', 'bInheritHandle']
+
+            _fields_ = [("nLength",DWORD),("lpSecurityDescriptor",LPVOID_32BIT),("bInheritHandle",BOOL)]
+
+            def writeToMemory(self, uc: Uc, address: int):
+                uc.mem_write(address, bytes(self))
+
+        class ARCH64(LittleEndianStructure):
+            types = ['DWORD', 'LPVOID', 'BOOL']
+            names = ['nLength', 'lpSecurityDescriptor', 'bInheritHandle']
+
+            _fields_ = [("nLength",DWORD),("lpSecurityDescriptor",LPVOID_64BIT),("bInheritHandle",BOOL)]
+
+            def writeToMemory(self, uc: Uc, address: int):
+                uc.mem_write(address, bytes(self))
+
+    # Struct value_entA
+    # Alias Names: VALENTA
+    # Alias Pointer Names: *PVALENTA
+
+    def get_value_entA(uc: Uc, address: int, em):
+            if em.arch == 32:
+                return Structure.value_entA.ARCH32.from_buffer_copy(uc.mem_read(address, sizeof(Structure.value_entA.ARCH32)))
+            else:
+                return Structure.value_entA.ARCH64.from_buffer_copy(uc.mem_read(address, sizeof(Structure.value_entA.ARCH64)))
+
+    # Struct Aliases:
+    get_VALENTA = get_value_entA
+
+    class value_entA:
+
+        class ARCH32(LittleEndianStructure):
+            types = ['LPSTR', 'DWORD', 'DWORD_PTR', 'DWORD']
+            names = ['ve_valuename', 've_valuelen', 've_valueptr', 've_type']
+
+            _fields_ = [("ve_valuename",LPSTR_32BIT),("ve_valuelen",DWORD),("ve_valueptr",DWORD_PTR),("ve_type",DWORD)]
+
+            def writeToMemory(self, uc: Uc, address: int):
+                uc.mem_write(address, bytes(self))
+
+        class ARCH64(LittleEndianStructure):
+            types = ['LPSTR', 'DWORD', 'DWORD_PTR', 'DWORD']
+            names = ['ve_valuename', 've_valuelen', 've_valueptr', 've_type']
+
+            _fields_ = [("ve_valuename",LPSTR_64BIT),("ve_valuelen",DWORD),("ve_valueptr",DWORD_PTR),("ve_type",DWORD)]
+
+            def writeToMemory(self, uc: Uc, address: int):
+                uc.mem_write(address, bytes(self))
