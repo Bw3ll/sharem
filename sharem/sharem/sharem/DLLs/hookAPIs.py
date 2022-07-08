2f2f99ce3d40027947dc8b9a38102c74128ee9b3
Ibranum <evan.read@trojans.dsu.edu>
2022-07-08 15:00:06 -0500

    CallNextHookEx function created

--- sharem/sharem/sharem/DLLs/hookAPIs.py
+++ sharem/sharem/sharem/DLLs/hookAPIs.py
@@ -9927,6 +9927,20 @@ class CustomWinAPIs():
         logged_calls= ("QueueUserAPC", hex(callAddr), (retValStr), 'DWORD', pVals, pTypes, pNames, False)
         return logged_calls, stackCleanup(uc, em, esp, len(pTypes))
 
+    def CallNextHookEx(self, uc: Uc, eip, esp, export_dict, callAddr, em):
+        pTypes= ['PAPCFUNC', 'HANLDE', 'ULONG_PTR']
+        pNames= ['pfnAPC', 'hThread', 'dwData']
+        pVals = makeArgVals(uc, em, esp, len(pTypes))
+
+        pTypes,pVals= findStringsParms(uc, pTypes,pVals, skip=[])
+
+        retVal = 0x1
+        retValStr= 'SUCCESS'
+        uc.reg_write(UC_X86_REG_EAX, retVal)     
+
+        logged_calls= ("CallNextHookEx", hex(callAddr), (retValStr), 'LRESULT', pVals, pTypes, pNames, False)
+        return logged_calls, stackCleanup(uc, em, esp, len(pTypes))
+
     def FindFirstUrlCacheEntryA(self, uc: Uc, eip, esp, export_dict, callAddr, em):
         pTypes= ['LPCSTR', 'LPINTERNET_CACHE_ENTRY_INFOA', 'LPDWORD']
         pNames= ['lpszUrlSearchPattern', 'lpFirstCacheEntryInfo', 'lpcbCacheEntryInfo']
