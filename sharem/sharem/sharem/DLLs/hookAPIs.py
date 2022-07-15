87c9cbbf7316d69eb0e1ff9ce10d3880c808af2e
Ibranum <evan.read@trojans.dsu.edu>
2022-07-15 11:41:40 -0500

    PeekMessageA Function Introduced

--- sharem/sharem/sharem/DLLs/hookAPIs.py
+++ sharem/sharem/sharem/DLLs/hookAPIs.py
@@ -10321,6 +10321,19 @@ class CustomWinAPIs():
         logged_calls= ("WNetAddConnectionA", hex(callAddr), (retValStr), 'DWORD', pVals, pTypes, pNames, False)
         return logged_calls, stackCleanup(uc, em, esp, len(pTypes))
 
+    def PeekMessageA(self, uc: Uc, eip, esp, export_dict, callAddr, em):
+        pTypes= ['LPMSG', 'HWND', 'UINT', 'UINT', 'UINT']
+        pNames= ['lpMsg', 'hWnd', 'wMsgFilterMin', 'wMsgFilterMax', 'wRemoveMsg']
+        pVals = makeArgVals(uc, em, esp, len(pTypes))
+
+        pTypes,pVals= findStringsParms(uc, pTypes,pVals, skip=[])
+
+        retVal = 0x1
+        retValStr= 'MESSAGE AVAILABLE'
+        uc.reg_write(UC_X86_REG_EAX, retVal)     
+
+        logged_calls= ("PeekMessageA", hex(callAddr), (retValStr), 'BOOL', pVals, pTypes, pNames, False)
+        return logged_calls, stackCleanup(uc, em, esp, len(pTypes))
 
     def DuplicateToken(self, uc: Uc, eip, esp, export_dict, callAddr, em):
         pTypes= ['HANDLE', 'SECURITY_IMPERSONATION_LEVEL', 'PHANDLE']
