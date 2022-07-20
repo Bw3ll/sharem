de295ba577c723bcd439222391d86ede3e98175a
Ibranum <evan.read@trojans.dsu.edu>
2022-07-20 11:26:04 -0500

    CryptDestroyHash function created

--- sharem/sharem/sharem/DLLs/hookAPIs.py
+++ sharem/sharem/sharem/DLLs/hookAPIs.py
@@ -10357,6 +10357,20 @@ class CustomWinAPIs():
         logged_calls= ("CryptReleaseContext", hex(callAddr), (retValStr), 'BOOL', pVals, pTypes, pNames, False)
         return logged_calls, stackCleanup(uc, em, esp, len(pTypes))
 
+    def CryptDestroyHash(self, uc: Uc, eip, esp, export_dict, callAddr, em):
+        pTypes= ['HCRYPTHASH']
+        pNames= ['hHash']
+        pVals = makeArgVals(uc, em, esp, len(pTypes))
+
+        pTypes,pVals= findStringsParms(uc, pTypes,pVals, skip=[])
+
+        retVal = 0x1
+        retValStr= 'SUCCESS'
+        uc.reg_write(UC_X86_REG_EAX, retVal)     
+
+        logged_calls= ("CryptDestroyHash", hex(callAddr), (retValStr), 'BOOL', pVals, pTypes, pNames, False)
+        return logged_calls, stackCleanup(uc, em, esp, len(pTypes))
+
 
     def GetIpNetTable(self, uc: Uc, eip, esp, export_dict, callAddr, em):
         pTypes= ['PMIB_IPNETTABLE', 'PULONG', 'BOOL']
