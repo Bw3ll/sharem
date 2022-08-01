6dff9c8d44120d9e78184063540a2a1f0e8712ac
ShelbyVH <77289500+ShelbyVH@users.noreply.github.com>
2022-08-01 12:26:46 -0500

    Fixed Brute-forced Printing on All APIs

--- sharem/sharem/sharem/helper/printingOutput.py
+++ sharem/sharem/sharem/helper/printingOutput.py
@@ -72,6 +72,7 @@ class PrintingOutput:
             retType = ret_type[t]
             paramVal = api_params_values[t]
             paramVal_tuple = api_params_values[t]
+            aBruteForce = api_bruteforce[t]
             # print(paramVal)
             for potentialTuple in paramVal:
                 if( type(potentialTuple) == tuple):
@@ -111,7 +112,7 @@ class PrintingOutput:
                     for ptyp, pname, pval in zip(pType, pName, paramVal):
                         text_output += '\t{} {} {}\n'.format(cya + ptyp, pname + ":" + res, pval)
                     text_output += "\t{} {}\n".format(red + "Return:" + res, retBundle)
-                if api_bruteforce:
+                if aBruteForce:
                     text_output += "\t{}\n\n".format(whi + "Brute-forced" + res, )
                 else:
                     text_output += "\n"
@@ -138,7 +139,7 @@ class PrintingOutput:
             retVal = ret_values[t]
             retType = ret_type[t]
             paramVal = syscall_params_values[t]
-            bruteForce = syscall_bruteforce[t]
+            sBruteForce = syscall_bruteforce[t]
             sid = syscallID[t]
             # DLL = dll_name[t]
             for potentialTuple in paramVal:
@@ -170,7 +171,7 @@ class PrintingOutput:
                         text_output += '\t{} {} {}\n'.format(cya + ptyp, pname + ":" + res, pval)
                     text_output += "\t{} {}\n".format(red + "Return:" + res, retBundle)
                 text_output += "\t{} {} - ({}, SP {})\n".format(red + "EAX: " + res, hex(sid) + res, em.winVersion + res, em.winSP + res)
-                if bruteForce:
+                if sBruteForce:
                     text_output += "\t{}\n\n".format(whi + "Brute-forced" + res, )
                 else:
                     text_output += "\n"
