9132dac9da8b6ecde55ff105d7e6454132b57fd4
dylan-park15 <dylan.park@trojans.dsu.edu>
2022-08-18 12:41:14 -0500

    Added GetMessage

--- sharem/sharem/sharem/DLLs/hookAPIs.py
+++ sharem/sharem/sharem/DLLs/hookAPIs.py
@@ -1312,6 +1312,21 @@ class CustomWinAPIs():
         logged_calls= ("GetDriveTypeA", hex(callAddr), (retValStr), 'UINT', pVals, pTypes, pNames, False)
         return logged_calls, stackCleanup(uc, em, esp, len(pTypes))
 
+    def GetMessage(self, uc: Uc, eip: int, esp: int, export_dict: dict, callAddr: int, em: EMU):
+        pTypes= ['LPMSG', 'HWND', 'UINT', 'UINT']
+        pNames= ['lpMsg', 'hWnd', 'wMsgFilterMin', 'wMsgFilterMax']
+        pVals = makeArgVals(uc, em, esp, len(pTypes))
+
+        # Might Need to Expand
+
+        pTypes,pVals= findStringsParms(uc, pTypes,pVals, skip=[])
+        retVal = 0x88888888
+
+        retValStr='True'
+        uc.reg_write(UC_X86_REG_EAX, retVal)     
+
+        logged_calls= ("GetMessage", hex(callAddr), (retValStr), 'BOOL', pVals, pTypes, pNames, False)
+        return logged_calls, stackCleanup(uc, em, esp, len(pTypes))
 
     def HeapCreate(self, uc: Uc, eip: int, esp: int, export_dict: dict, callAddr: int, em: EMU):
         # HANDLE HeapCreate([in] DWORD  flOptions,[in] SIZE_T dwInitialSize,[in] SIZE_T dwMaximumSize);
