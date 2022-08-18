d484f167b2650ec9f14af92e9e465afc3988b7a6
dylan-park15 <dylan.park@trojans.dsu.edu>
2022-08-18 12:55:24 -0500

    Added ExpandEnvironmentStrings

--- sharem/sharem/sharem/DLLs/hookAPIs.py
+++ sharem/sharem/sharem/DLLs/hookAPIs.py
@@ -1312,7 +1312,7 @@ class CustomWinAPIs():
         logged_calls= ("GetDriveTypeA", hex(callAddr), (retValStr), 'UINT', pVals, pTypes, pNames, False)
         return logged_calls, stackCleanup(uc, em, esp, len(pTypes))
 
-    def GetMessage(self, uc: Uc, eip: int, esp: int, export_dict: dict, callAddr: int, em: EMU):
+    def GetMessageA(self, uc: Uc, eip: int, esp: int, export_dict: dict, callAddr: int, em: EMU):
         pTypes= ['LPMSG', 'HWND', 'UINT', 'UINT']
         pNames= ['lpMsg', 'hWnd', 'wMsgFilterMin', 'wMsgFilterMax']
         pVals = makeArgVals(uc, em, esp, len(pTypes))
@@ -1325,7 +1325,23 @@ class CustomWinAPIs():
         retValStr='True'
         uc.reg_write(UC_X86_REG_EAX, retVal)     
 
-        logged_calls= ("GetMessage", hex(callAddr), (retValStr), 'BOOL', pVals, pTypes, pNames, False)
+        logged_calls= ("GetMessageA", hex(callAddr), (retValStr), 'BOOL', pVals, pTypes, pNames, False)
+        return logged_calls, stackCleanup(uc, em, esp, len(pTypes))
+
+    def xxExpandEnvironemtnStringsA(self, uc: Uc, eip: int, esp: int, export_dict: dict, callAddr: int, em: EMU):
+        pTypes= ['LPCSTR', 'LPSTR', 'DWORD']
+        pNames= ['lpSrc', 'lpDst', 'nSize']
+        pVals = makeArgVals(uc, em, esp, len(pTypes))
+
+        # Might Need to Expand
+
+        pTypes,pVals= findStringsParms(uc, pTypes,pVals, skip=[])
+        retVal = 0x88888888
+
+        retValStr='True'
+        uc.reg_write(UC_X86_REG_EAX, retVal)     
+
+        logged_calls= ("ExpandEnvironemtnStringsA", hex(callAddr), (retValStr), 'DWORD', pVals, pTypes, pNames, False)
         return logged_calls, stackCleanup(uc, em, esp, len(pTypes))
 
     def HeapCreate(self, uc: Uc, eip: int, esp: int, export_dict: dict, callAddr: int, em: EMU):
